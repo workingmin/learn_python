a1a8c39609a3bf66379916bd42b8546c74c5ac7d
wangjianzheng <wangjianzheng@xhqb.com>
2022-06-21 18:01:52 +0800

    add gitlab api demo

---
+++ gitlab_api/get_project_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import yaml
+from gitlab import Gitlab
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: " + sys.argv[0] + " <yaml_config>")
+        print(" example: " + sys.argv[0] + " config.yml")
+        sys.exit(0)
+        
+    yaml_config = sys.argv[1]
+    with open(yaml_config, 'r') as f:
+        data = f.read()
+        config = yaml.load(data, Loader=yaml.FullLoader)
+        url = config['url']
+        private_token = config['private_token']
+        project_name = config['project_name']
+                
+    gl = Gitlab(url=url, private_token=private_token)
+    project = None
+    try:
+        project = gl.projects.get(project_name)
+    except Exception as e:
+        print(e)
+    
+    if project is not None:
+        print(project)
---
+++ gitlab_api/search_project_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+from tokenize import group
+import yaml
+import gitlab
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: " + sys.argv[0] + " <yaml_config>")
+        print(" example: " + sys.argv[0] + " config.yml")
+        sys.exit(0)
+        
+    yaml_config = sys.argv[1]
+    with open(yaml_config, 'r') as f:
+        data = f.read()
+        config = yaml.load(data, Loader=yaml.FullLoader)
+        url = config['url']
+        private_token = config['private_token']
+        group_name = config['group_name']
+        project_name = config['project_name']
+                
+    gl = gitlab.Gitlab(url=url, private_token=private_token)
+    project_id = None
+    try:
+        for item in gl.search(gitlab.const.SEARCH_SCOPE_PROJECTS, project_name):
+            if item['namespace']['full_path'] == group_name and item['name'] == project_name:
+                project_id = item['id']
+                break
+    except Exception as e:
+        print(e)
+        
+    if project_id is not None:
+        project = gl.projects.get(project_id)
+        print(project)
